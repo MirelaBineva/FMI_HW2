97d9d03a98a868d416a04b05c26614a338cd7fdd
MirelaBineva <33381175+MirelaBineva@users.noreply.github.com>
2018-01-04 23:11:43 +0200

    Add files via upload

---
+++ zad10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "How many guests are there? ";
+    cin >> N;
+    if (N>=40 && N<=10040)
+    {
+        int K;
+        cout <<"Max capacity? ";
+        cin >>K;
+        if (K>=10 && K<=20)
+            {
+
+
+
+
+
+
+            }
+    }
+
+
+    return 0;
+}
